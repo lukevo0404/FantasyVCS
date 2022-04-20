96f89817c2cbe589423d099f71d795d4b72f819f
vinhdo2001 <57248915+vinhdo2001@users.noreply.github.com>
2022-04-20 11:01:19 +0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import SignIn from './Pages/SignIn';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/sign-up" element={<SignIn/>}></Route>
         </Routes>
       </Router>
     </>
