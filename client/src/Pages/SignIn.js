d5e879eda4d11dc2c4162f663257dfb683dfab89
vinhdo2001 <57248915+vinhdo2001@users.noreply.github.com>
2022-04-20 11:01:27 +0700

    Create SignIn.js

---
+++ client/src/Pages/SignIn.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from './Home/Hero copy'
+
+import './Home/Home.css'
+
+
+function SignIn() {
+    return (
+        <>
+            <img src='/images/background.svg' id='background' />
+            <Hero />
+            
+            
+        </>
+    )
+}
+
+export default SignIn
