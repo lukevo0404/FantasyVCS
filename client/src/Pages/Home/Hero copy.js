32bcff113ae8d04a780afaabd2aeb4142ce462f8
vinhdo2001 <57248915+vinhdo2001@users.noreply.github.com>
2022-04-20 11:01:25 +0700

    Create Hero copy.js

---
+++ client/src/Pages/Home/Hero copy.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from '../../components/Navbar'
+import './Hero copy.css'
+
+function Hero() {
+  return (
+    <>
+    <div className="hero-container">
+        <Navbar/>
+        <div className="title-text-wrapper">
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Hero
