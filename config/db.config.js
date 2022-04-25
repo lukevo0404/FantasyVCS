3ebc0a4d80f510710a317a225a7201750c5bc148
timmyha619 <timmyha619@yahoo.com>
2022-04-25 11:36:52 +0700

    new unfinished  backend + successfully connect to mongodb

--- config/db.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  HOST: "localhost",
-  PORT: 27017,
-  DB: "DucDB",
-  url: "mongodb+srv://timmyha619:Bapcai2312!@ducdb.kjkro.mongodb.net/FantasyVCS?retryWrites=true&w=majority", //change to the group's db
-};
--- event/event.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require ('mongoose')
-
-const eventSchema = mongoose.Schema ({
-    userID: {type: String,default:null},
-    date: {type: String, default:null},
-    room: {type: String, default: null}
-})
-
-module.exports = mongoose.model("event", eventSchema);
