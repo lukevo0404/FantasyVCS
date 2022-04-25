c418fbcfec2cf4467305620be0065e7dbdcf5d03
vinhdo2001 <57248915+vinhdo2001@users.noreply.github.com>
2022-04-25 08:11:00 +0700

    Update SignIn.js

--- client/src/Pages/SignIn.js
+++ client/src/Pages/SignIn.js
@@ -2,11 +2,18 @@ import React, { useEffect, useState } from 'react'
 import Hero from './Home/Hero copy'
 import { Link, useHistory } from "react-router-dom";
 import './Home/Home.css'
-
-
+import './Login.css'
+import { useAuthState } from "../../node_modules/react-firebase-hooks/auth";
+import { auth, signInWithEmailAndPassword, signInWithGoogle } from "./firebase";
 function SignIn() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [user, loading, error] = useAuthState(auth);
+    
+    const history = useHistory();
+    useEffect(() => {
+        if (user) history.replace("/");
+    }, [user, loading]);
     return (
         <>
             <img src='/images/hero-image.svg' id='background' />
@@ -29,16 +36,19 @@ function SignIn() {
                     />
                     <button
                     className="login__btn"
+                    onClick={() => signInWithEmailAndPassword(email, password)}
                     >
                     Login
                     </button>
-                    <button className="login__btn login__google">
+                    <button className="login__btn login__google" onClick={signInWithGoogle}>
                     Login with Google
                     </button>
                     <div>
                     <Link to="/reset">Forgot Password</Link>
                     </div>
-                    
+                    <div>
+                    Don't have an account? <Link to="/register">Register</Link> now.
+                    </div>
                 </div>
             </div> 
         </>
