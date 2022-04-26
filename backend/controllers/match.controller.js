63fe5759071c38647862f73a4eaf89dfcf9634ce
timmyha619 <timmyha619@yahoo.com>
2022-04-26 21:40:40 +0700

    add mathc controller

--- backend/controllers/match.controller.js
+++ backend/controllers/match.controller.js
@@ -1,11 +1,69 @@
-const Match = require('../models/match.model');
+const db = require ("../models/match.model");
+const Match = db.match;
 
-class matchController {
-    index(req, res, next) {
-        Match.find({})
-        .then(matches => res.json(matches))
-        .catch(next);
-    }
-}
+//Get Match
+exports.get = (req, res) => {
+    const id = req.params.id;
 
-module.exports = new matchController();
+  Match.findbyId(id)
+    .then((data) => {
+      if (!data)
+        res
+          .status(404)
+          .send({ message: "Not found information with id " + id });
+      else res.send(data);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving match information with id=" + id });
+    });
+};
+
+// Update Match
+exports.update = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!",
+    });
+  }
+
+  const id = req.params.id;
+
+  Match.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot update!`,
+        });
+      } else res.send({ message: "Updated successfully." });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error updating!",
+      });
+    });
+};
+
+// Delete Match
+exports.delete = (req, res) => {
+  const id = req.params.id;
+
+  Match.findByIdAndRemove(id)
+    .then((data) => {
+      if (!data) {
+        res.status(404).send({
+          message: `Cannot delete!`,
+        });
+      } else {
+        res.send({
+          message: "Deleted successfully!",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Could not delete!",
+      });
+    });
+};
