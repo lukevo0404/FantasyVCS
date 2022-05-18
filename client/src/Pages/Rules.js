5ff960620b310b1705bcc4b01408d47192f64c01
vinhdo2001 <57248915+vinhdo2001@users.noreply.github.com>
2022-05-18 14:36:43 +0700

    Update Rules.js

--- client/src/Pages/Rules.js
+++ client/src/Pages/Rules.js
@@ -14,15 +14,38 @@ function Rules() {
                 </h5>
                 <h5>Squad Size
                 </h5>
-                <p>To join the game select a fantasy League of Legends squad of 10 players, consisting of:
+                <p>To join the game select a fantasy League of Legends squad of 5 players, consisting of:
                 </p>
                 <ul>
-                    <li>2 Top laners</li>
-                    <li>2 Junglers</li>
-                    <li>2 Mid laners</li>
-                    <li>2 Bot laners</li>
-                    <li>2 Supports</li>
+                    <li>1 Top laners</li>
+                    <li>1 Junglers</li>
+                    <li>1 Mid laners</li>
+                    <li>1 Bot laners</li>
+                    <li>1 Supports</li>
                 </ul>
+                <h5>Scoring</h5>
+                <table>
+                    <tr>
+                        <th>Action</th>
+                        <th>Points</th>
+                    </tr>
+                    <tr>
+                        <th>For every 2 kills</th>
+                        <th>1</th>
+                    </tr>
+                    <tr>
+                        <th>For every deaths</th>
+                        <th>-1</th>
+                    </tr>
+                    <h1>KDA</h1>
+                    <tr>
+                        <th>For every 3.0 KDA</th>
+                        <th>1</th>
+                    </tr>
+                    <tr>
+                        <th></th>
+                    </tr>
+                </table>
             
             
         </div>
